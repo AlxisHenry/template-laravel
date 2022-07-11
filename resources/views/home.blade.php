a0df4a83f31634d0846d6cea6ac1ccb5a500dcf8
AlxisHenry <alexis.henry150357@gmail.com>
2022-07-11 21:28:31 +0200

    Update homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,22 @@
 
 @section('body')
 
+    <section class="layout">
+
+        <div class="container">
+
+            <div class="container-text">
+                Welcome to our application,<br>
+                Build with Laravel.
+            </div>
+            <div class="container-asset">
+                <img src="{{ url('assets/svg/homepage_view.svg') }}" alt="">
+            </div>
+
+        </div>
+
+    </section>
+
 @endsection
 
 @section('scripts')
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,28 @@
+<nav class="navbar">
+
+    <div class="navbar_container">
+        <div class="navbar_content">
+            <div class="navbar_title">
+                <span class="main mb-2">{{ "CCI Campus " }}</span>&nbsp;<span class="secondary">web application</span>
+            </div>
+            <div class="contain_navbar_redirections">
+                <div class="navbar_redirections">
+                    <a href="/project" class="underlined">Project</a>
+                    <a target="_blank" href="https://github.com/AlxisHenry/alexishenry.eu" class="underlined">Github</a>
+                </div>
+                <x-btn-primary text="Sign-in" icon='<i class="fa-solid fa-arrow-right-long"></i>' to="/login" background="true"/>
+            </div>
+            <div class="navbar_burger">
+                <i class="fa-solid fa-bars"></i>
+            </div>
+        </div>
+
+        <div class="navbar_burger_content">
+            <a href="/project" tabindex="-1" class="hover-effec">Project</a>
+            <a target="_blank" tabindex="-1" class="hover-effec" href="https://github.com/AlxisHenry/alexishenry.eu">Github</a>
+            <a href="/login" tabindex="-1" class="hover-effec">Sign-in</a>
+        </div>
+
+    </div>
+
+</nav>
