aa9a480980ae2c2eaae17aa9cc9ebf218b9f5c1e
AlxisHenry <alexis.henry150357@gmail.com>
2022-07-12 06:44:20 +0200

    Change name of login page

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,4 @@ use App\Http\Controllers\MainController;
 // Add custom routes below
 
 Route::get('/', [MainController::class, 'main'])->name('homepage');
-Route::get('/login', [MainController::class, 'main'])->name('homepage');
+Route::get('/login', [MainController::class, 'main'])->name('login');
