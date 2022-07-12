1097efd7aa733643f15f6dc733c9845f200ce494
AlxisHenry <alexis.henry150357@gmail.com>
2022-07-12 21:50:02 +0200

    Provide an auth system

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -3,14 +3,18 @@
     <div class="navbar_container">
         <div class="navbar_content">
             <div class="navbar_title">
-                <span class="main mb-2">{{ "CCI Campus " }}</span>&nbsp;<span class="secondary">web application</span>
+                <span class="main mb-2">CCI Campus</span>&nbsp;<span class="secondary">web application</span>
             </div>
             <div class="contain_navbar_redirections">
                 <div class="navbar_redirections">
                     <a href="/project" class="underlined">Project</a>
                     <a target="_blank" href="https://github.com/AlxisHenry/alexishenry.eu" class="underlined">Github</a>
                 </div>
-                <x-btn-primary text="Sign-in" icon='<i class="fa-solid fa-arrow-right-long"></i>' to="/login" background="true"/>
+                @if(Auth::check())
+                    <x-btn-primary text="Profile" icon='<i class="fa-solid fa-arrow-right-long"></i>' to="/account" background="true"/>
+                @else
+                    <x-btn-primary text="Sign-in" icon='<i class="fa-solid fa-arrow-right-long"></i>' to="/login" background="true"/>
+                @endif
             </div>
             <div class="navbar_burger">
                 <i class="fa-solid fa-bars"></i>
@@ -20,7 +24,11 @@
         <div class="navbar_burger_content">
             <a href="/project" tabindex="-1" class="hover-effect">Project</a>
             <a target="_blank" tabindex="-1" class="hover-effect" href="https://github.com/AlxisHenry/alexishenry.eu">Github</a>
-            <a href="/login" tabindex="-1" class="hover-effect">Sign-in</a>
+            @if(Auth::check())
+                    <a href="/account" tabindex="-1" class="hover-effect">Profile</a>
+            @else
+                <a href="/login" tabindex="-1" class="hover-effect">Sign-in</a>
+            @endif
         </div>
 
     </div>
