d7485b872c2b6122f60c61f43b1cb44171ec8381
AlxisHenry <alexis.henry150357@gmail.com>
2022-07-12 21:50:45 +0200

    ui design footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,29 @@
+<footer>
+
+    <div class="footer_top">
+
+        <div class="footer_left">
+            <!--
+                  todo => Links
+            -->
+
+        </div>
+
+        <div class="footer_right">
+            <!--
+               todo => Inscription with email to newsletter
+            -->
+        </div>
+
+    </div>
+
+    <div class="footer_bottom">
+        <span>
+            <a href="">Privacy Policy</a> &nbsp; &bull; &nbsp; <a href="">Terms and conditions</a>
+        </span>
+        <span class="social-networks">
+            <a href="">Linkedin</a> &nbsp; &bull; &nbsp; <a href="">Github</a>
+        </span>
+    </div>
+
+</footer>
