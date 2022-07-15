452405810526145914dceec0e18b409c05cebcc8
AlxisHenry <alexis.henry150357@gmail.com>
2022-07-15 10:33:15 +0200

    Refactor navbar. Use component in burger menu.

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,5 +1,4 @@
 <nav class="navbar">
-
     <div class="navbar_container">
         <div class="navbar_content">
             <div class="navbar_title">
@@ -13,27 +12,17 @@
                     <a href="/project" class="underlined">Project</a>
                     <a target="_blank" href="https://github.com/AlxisHenry/alexishenry.eu" class="underlined">Github</a>
                 </div>
-                @if(Auth::check())
-                    <x-btn-primary text="Profile" icon='<i class="fa-solid fa-arrow-right-long"></i>' to="/account" background="true"/>
-                @else
-                    <x-btn-primary text="Sign-in" icon='<i class="fa-solid fa-arrow-right-long"></i>' to="/login" background="true"/>
-                @endif
+                <x-btn-primary text="{{Auth::check() ? 'Profile' : 'Sign-in' }}" icon='<i class="fa-solid fa-arrow-right-long"></i>' to="/{{ Auth::check() ? 'profile' : 'sign-in' }}" background="true"/>
             </div>
             <div class="navbar_burger">
                 <i class="fa-solid fa-bars"></i>
             </div>
         </div>
-
         <div class="navbar_burger_content">
+            <a href="/" tabindex="-1" class="hover-effect">Home</a>
             <a href="/project" tabindex="-1" class="hover-effect">Project</a>
             <a target="_blank" tabindex="-1" class="hover-effect" href="https://github.com/AlxisHenry/alexishenry.eu">Github</a>
-            @if(Auth::check())
-                    <a href="/account" tabindex="-1" class="hover-effect">Profile</a>
-            @else
-                <a href="/login" tabindex="-1" class="hover-effect">Sign-in</a>
-            @endif
+            <x-btn-primary text="{{Auth::check() ? 'Profile' : 'Sign-in' }}" icon='<i class="fa-solid fa-arrow-right-long"></i>' to="/{{ Auth::check() ? 'profile' : 'sign-in' }}"/>
         </div>
-
     </div>
-
 </nav>
