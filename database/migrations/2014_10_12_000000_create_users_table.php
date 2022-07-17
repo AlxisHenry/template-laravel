1563dbaeda4dc06ae188644ddf7798aa3659426c
AlxisHenry <alexis.henry150357@gmail.com>
2022-07-17 20:15:51 +0200

    Users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('zip')->nullable();
+            $table->string('city')->nullable();
+            $table->string('address')->nullable();
+            $table->string('country')->nullable();
+            # todo => Add optional birthday date
             $table->rememberToken();
             $table->timestamps();
         });
