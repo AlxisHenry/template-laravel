f98f38bd7fcbeb5722d6c237b9f5b3cc723d3315
AlxisHenry <alexis.henry150357@gmail.com>
2022-07-17 20:20:38 +0200

    Sign-in/Sign-up routes

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomepageController;
-use App\Http\Controllers\LoginController;
+use App\Http\Controllers\AuthController;
 
 // Add custom routes below
 
 Route::get('/', [HomepageController::class, 'homepage'])->name('home');
-Route::get('/login', [LoginController::class, 'login'])->name('login');
+Route::get('/sign-in', [AuthController::class, 'SignIn'])->name('login');
+Route::get('/sign-up', [AuthController::class, 'SignUp'])->name('register');
