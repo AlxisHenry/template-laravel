d68abc2e19bdc40ce2383bf0d7fcc64fd9fe84af
AlxisHenry <alexis.henry150357@gmail.com>
2022-07-18 11:56:37 +0200

    Add routes (GET & POST methods) for auth

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,5 @@ use App\Http\Controllers\AuthController;
 Route::get('/', [HomepageController::class, 'homepage'])->name('home');
 Route::get('/sign-in', [AuthController::class, 'SignIn'])->name('login');
 Route::get('/sign-up', [AuthController::class, 'SignUp'])->name('register');
+Route::get('/auth/{type}', function () { return abort('404'); });
+Route::post('/auth/{type}', [AuthController::class, 'Auth'])->name('auth');
