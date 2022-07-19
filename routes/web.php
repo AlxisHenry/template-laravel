95b9442be6d9db2742848f7a9c30a1672de911c2
AlxisHenry <alexis.henry150357@gmail.com>
2022-07-19 13:43:29 +0200

    Refactor web.php file. Add comments to routes.

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,37 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomepageController;
+use App\Http\Controllers\GlobalController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
-// Add custom routes below
+/*
+ * Routes without restrictions
+ * */
 
-Route::get('/', [HomepageController::class, 'homepage'])->name('home');
-Route::get('/sign-in', [AuthController::class, 'SignIn'])->name('login');
-Route::get('/sign-up', [AuthController::class, 'SignUp'])->name('register');
-Route::get('/auth/{type}', function () { return abort('404'); });
-Route::post('/auth/{type}', [AuthController::class, 'Auth'])->name('auth');
+# GlobalController doesn't use any middleware
+Route::get('/', [GlobalController::class, 'Homepage'])->name('global.homepage');
+Route::get('/project', [GlobalController::class, 'Project'])->name('global.project');
+Route::get('/legals', [GlobalController::class, 'Legals'])->name('global.legals');
+Route::get('/authors', [GlobalController::class, 'Authors'])->name('global.authors');
+
+/*
+ * Authenticate
+ * */
+
+# AuthController use the auth.system middleware
+Route::get('/sign-in', [AuthController::class, 'SignIn'])->name('auth.login'); # Redirect to sign-in form
+Route::get('/sign-up', [AuthController::class, 'SignUp'])->name('auth.register'); # Redirect to sign-up form
+Route::post('/auth/{type}', [AuthController::class, 'Auth'])->name('auth.check.post'); # Check authenticate action
+Route::get('/auth/{type}', function () { return abort('404'); })->name('auth.check.get'); # Abort GET method for this route
+
+/*
+ * User account
+ * */
+
+# UserController use multiples middleware
+Route::get('/profile', [UserController::class, 'Profile'])->name('user.profile'); # Redirect to user profile
+Route::get('/profile/settings', [UserController::class, 'SignIn'])->name('user.profile.settings'); # Settings of user account
+
+# Add custom routes below
+# For example: Route::get('uri', [NameController::class, 'functionName'])->name(routeName)
