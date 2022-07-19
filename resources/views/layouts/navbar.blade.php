0a345bbbe3e043ac30582163449fdee561545fab
AlxisHenry <alexis.henry150357@gmail.com>
2022-07-19 14:45:13 +0200

    Add custom class to elements / Refactor primary btn component tag

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -9,20 +9,20 @@
             </div>
             <div class="contain_navbar_redirections">
                 <div class="navbar_redirections">
-                    <a href="/project" class="underlined">Project</a>
-                    <a target="_blank" href="https://github.com/AlxisHenry/alexishenry.eu" class="underlined">Github</a>
+                    <a href="/project" class="underlined select-none">Project</a>
+                    <a target="_blank" href="https://github.com/AlxisHenry/alexishenry.eu" class="underlined select-none">Github</a>
                 </div>
-                <x-btn-primary text="{{Auth::check() ? 'Profile' : 'Sign-in' }}" icon='<i class="fa-solid fa-arrow-right-long"></i>' to="/{{ Auth::check() ? 'profile' : 'sign-in' }}" background="true"/>
+                <x-btn-primary content="{{Auth::check() ? 'Profile' : 'Sign-in' }}" url="/{{ Auth::check() ? 'profile' : 'sign-in' }}" background="true"/>
             </div>
             <div class="navbar_burger">
                 <i class="fa-solid fa-bars"></i>
             </div>
         </div>
         <div class="navbar_burger_content">
-            <a href="/" tabindex="-1" class="hover-effect">Home</a>
-            <a href="/project" tabindex="-1" class="hover-effect">Project</a>
-            <a target="_blank" tabindex="-1" class="hover-effect" href="https://github.com/AlxisHenry/alexishenry.eu">Github</a>
-            <x-btn-primary text="{{Auth::check() ? 'Profile' : 'Sign-in' }}" icon='<i class="fa-solid fa-arrow-right-long"></i>' to="/{{ Auth::check() ? 'profile' : 'sign-in' }}"/>
+            <a href="/" tabindex="-1" class="grayscale select-none">Home</a>
+            <a href="/project" tabindex="-1" class="grayscale select-none">Project</a>
+            <a target="_blank" tabindex="-1" class="grayscale select-none" href="https://github.com/AlxisHenry/alexishenry.eu">Github</a>
+            <x-btn-primary content="{{Auth::check() ? 'Profile' : 'Sign-in' }}" url="/{{ Auth::check() ? 'profile' : 'sign-in' }}" background="true"/>
         </div>
     </div>
 </nav>
