fad76f5f9cbfb86b605663f2cd06f61f68b9b4b8
AlxisHenry <alexis.henry150357@gmail.com>
2022-07-19 13:47:20 +0200

    Refactor Primary Button Component

--- app/View/Components/BtnPrimary.php
+++ app/View/Components/BtnPrimary.php
@@ -3,38 +3,51 @@
 namespace App\View\Components;
 
 use Illuminate\View\Component;
+use \Illuminate\Contracts\View\View;
+
+/*
+ * This component is a primary button with default style (path: scss\components\btn-primary.scss)
+ * You may call this component with the following tag:
+ * <x-btn-primary content="content" fontawesome='your icon|default arrow' url="/" background="true|false"/>
+ * */
 
 class BtnPrimary extends Component
 {
 
-    public string $text;
-    public string $icon;
-    public string $className;
-    public bool $background;
-    public string $to;
+    public string $content; # Value of the button
+    public string $fontawesome; # You may use a font awesome icon only
+    public string $className; # You can assign one or more classes to the button
+    public bool $background; # Bool for check if the button take background color or not
+    public string $url; # Choose the redirection url
 
     /**
      * Create a new component instance.
      *
      * @return void
      */
-    public function __construct($text, $to = '', $background = false, $icon = '')
+    public function __construct(
+        $content = 'Hello',
+        $fontawesome = '<i class="fa-solid fa-arrow-right-long"></i>',
+        $className = '',
+        $background = true,
+        $url = '/')
     {
-        $this->text = $text;
-        $this->icon = $icon;
-        $this->background = $background;
-        $this->className = strtolower($text);
-        $this->to = $to;
+        $this->content = $content;
+        $this->fontawesome = $fontawesome;
+        $this->className = strtolower($className);
+        $this->background = $background === "true" ? true : ($background === "false" ? false : ''); # Set background to true|false
+        $this->url = $url;
 
     }
 
     /**
      * Get the view / contents that represent the component.
      *
-     * @return \Illuminate\Contracts\View\View|\Closure|string
+     * @return View|Closure|string
      */
-    public function render(): \Illuminate\Contracts\View\View|string|\Closure
+    public function render(): View|string|\Closure
     {
+        # When you call the component with the tag, this class will return you the following component view with your parameters
         return view('components.btn-primary',
             [
                 'background' => $this->background
--- resources/views/components/btn-primary.blade.php
+++ resources/views/components/btn-primary.blade.php
@@ -1,6 +1,8 @@
+{{-- This file is the view of component : app\View\Components\BtnPrimary.php  --}}
+
 <div class="btn btn-primary btn-{{ $className }}">
-    <a class="btn-link {{ $background ? '' : 'no-bg' }}" href="{{$to}}">
-        {{ $text }}
-        {!! $icon !!}
+    <a class="btn-link {{ $background ? '' : 'no-bg' }}" href="{{$url}}">
+        {{ $content }}
+        {!! $fontawesome !!}
     </a>
 </div>
