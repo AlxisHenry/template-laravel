6a55eead2ca107658b34fe4e2489935850cd4fbc
AlxisHenry <alexis.henry150357@gmail.com>
2022-07-19 13:49:12 +0200

    Create a GlobalController.php for routes who doesn't use middleware

--- app/Http/Controllers/GlobalController.php
+++ app/Http/Controllers/GlobalController.php
@@ -4,14 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HomepageController extends Controller
+class GlobalController extends Controller
 {
-
     public function Homepage(): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
     {
         return view('home', [
             'title' => 'Home'
         ]);
     }
-
 }
